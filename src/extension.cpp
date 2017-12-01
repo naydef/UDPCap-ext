331e5e8849a9dceed9ce7916d7ee5fbee89bfdc7
naydef <naydel9@hotmail.com>
2017-12-01 20:16:32 +0200

    Identation fix

--- src/extension.cpp
+++ src/extension.cpp
@@ -1,33 +1,33 @@
 /**
- * vim: set ts=4 :
- * =============================================================================
- * SourceMod Sample Extension
- * Copyright (C) 2004-2008 AlliedModders LLC.  All rights reserved.
- * =============================================================================
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, version 3.0, as published by the
- * Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * As a special exception, AlliedModders LLC gives you permission to link the
- * code of this program (as well as its derivative works) to "Half-Life 2," the
- * "Source Engine," the "SourcePawn JIT," and any Game MODs that run on software
- * by the Valve Corporation.  You must obey the GNU General Public License in
- * all respects for all other code used.  Additionally, AlliedModders LLC grants
- * this exception to all derivative works.  AlliedModders LLC defines further
- * exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
- * or <http://www.sourcemod.net/license.php>.
- *
- * Version: $Id$
- */
+* vim: set ts=4 :
+* =============================================================================
+* SourceMod Sample Extension
+* Copyright (C) 2004-2008 AlliedModders LLC.  All rights reserved.
+* =============================================================================
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, version 3.0, as published by the
+* Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* As a special exception, AlliedModders LLC gives you permission to link the
+* code of this program (as well as its derivative works) to "Half-Life 2," the
+* "Source Engine," the "SourcePawn JIT," and any Game MODs that run on software
+* by the Valve Corporation.  You must obey the GNU General Public License in
+* all respects for all other code used.  Additionally, AlliedModders LLC grants
+* this exception to all derivative works.  AlliedModders LLC defines further
+* exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
+* or <http://www.sourcemod.net/license.php>.
+*
+* Version: $Id$
+*/
 
 #include "extension.h"
 #include "inetchannel.h"
@@ -35,9 +35,9 @@
 #include "windows.h"
 
 /**
- * @file extension.cpp
- * @brief Implement extension code here.
- */
+* @file extension.cpp
+* @brief Implement extension code here.
+*/
 
 UDPCap g_Sample;		/**< Global singleton for extension's main interface */
 
@@ -50,52 +50,52 @@ IGameConfig *g_pGameConf = nullptr;
 
 DETOUR_DECL_MEMBER1(Detour_ProcessPacket, bool, netpacket_t*, packet)
 {
-    if(!g_pProcessPacket)
-    {
-        return DETOUR_MEMBER_CALL(Detour_ProcessPacket)(packet); // This is not supposed to happen normally. Generally speaking, don't mess with destiny.
-    }
-    cell_t result=0;
+	if(!g_pProcessPacket)
+	{
+		return DETOUR_MEMBER_CALL(Detour_ProcessPacket)(packet); // This is not supposed to happen normally. Generally speaking, don't mess with destiny.
+	}
+	cell_t result=0;
 
-    g_pProcessPacket->PushString(packet->from.ToString());
-    g_pProcessPacket->PushCell(packet->from.port);
-    g_pProcessPacket->PushString((char*)packet->data);
-    g_pProcessPacket->PushCell(packet->size);
-    g_pProcessPacket->PushCell(packet->from.type);
+	g_pProcessPacket->PushString(packet->from.ToString());
+	g_pProcessPacket->PushCell(packet->from.port);
+	g_pProcessPacket->PushString((char*)packet->data);
+	g_pProcessPacket->PushCell(packet->size);
+	g_pProcessPacket->PushCell(packet->from.type);
 
-    g_pProcessPacket->Execute(&result);
+	g_pProcessPacket->Execute(&result);
 
-    if(result==Pl_Handled)
-    {
-        return 1;
-    }
-    return DETOUR_MEMBER_CALL(Detour_ProcessPacket)(packet);
+	if(result==Pl_Handled)
+	{
+		return 1;
+	}
+	return DETOUR_MEMBER_CALL(Detour_ProcessPacket)(packet);
 }
 
 bool UDPCap::SDK_OnLoad(char *error, size_t maxlength, bool late)
 {
-    if(!gameconfs->LoadGameConfigFile("udp-cap", &g_pGameConf, error, maxlength))
-    {
-        return false;
-    }
-    SM_GET_IFACE(MEMORYUTILS, memutils);
+	if(!gameconfs->LoadGameConfigFile("udp-cap", &g_pGameConf, error, maxlength))
+	{
+		return false;
+	}
+	SM_GET_IFACE(MEMORYUTILS, memutils);
 	CDetourManager::Init(g_pSM->GetScriptingEngine(), g_pGameConf);
 
-    g_pDetour=DETOUR_CREATE_MEMBER(Detour_ProcessPacket, "CBaseServer::ProcessConnectionlessPacket");
+	g_pDetour=DETOUR_CREATE_MEMBER(Detour_ProcessPacket, "CBaseServer::ProcessConnectionlessPacket");
 
-    if(g_pDetour==nullptr)
+	if(g_pDetour==nullptr)
 	{
 		snprintf(error, maxlength, "Error loading detour ProcessPacket!");
 		return false;
 	}
 	g_pDetour->EnableDetour();
-    g_pProcessPacket = forwards->CreateForward("UDPC_ProcessIncomingPacket", ET_Event, 5, NULL, Param_String, Param_Cell, Param_String, Param_Cell, Param_Cell);
+	g_pProcessPacket = forwards->CreateForward("UDPC_ProcessIncomingPacket", ET_Event, 5, NULL, Param_String, Param_Cell, Param_String, Param_Cell, Param_Cell);
 
 	return true;
 }
 
 void UDPCap::SDK_OnUnload()
 {
-    g_pDetour->Destroy();
-    gameconfs->CloseGameConfigFile(g_pGameConf);
-    forwards->ReleaseForward(g_pProcessPacket);
+	g_pDetour->Destroy();
+	gameconfs->CloseGameConfigFile(g_pGameConf);
+	forwards->ReleaseForward(g_pProcessPacket);
 }
